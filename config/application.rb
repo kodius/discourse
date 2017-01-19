e6f5cb035b29c65747d77b29c10c7be8ba69c99c
drKreso <kresimir.bojcic@gmail.com>
2017-01-19 12:55:36 +0100

    Normal database.yml config

--- config/application.rb
+++ config/application.rb
@@ -27,11 +27,11 @@ module Discourse
   class Application < Rails::Application
 
     def config.database_configuration
-      if Rails.env.production?
-        GlobalSetting.database_config
-      else
+      #if Rails.env.production?
+        #GlobalSetting.database_config
+      #else
         super
-      end
+      #end
     end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
