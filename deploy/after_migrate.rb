17ae6da9d8c1fcc1744939e4ecde55554aa082ac
drKreso <kresimir.bojcic@gmail.com>
2017-01-23 11:45:20 +0100

    ENV

--- deploy/after_migrate.rb
+++ deploy/after_migrate.rb
@@ -1,7 +1,16 @@
-rails_env = new_resource.environment["RAILS_ENV"]
+def self.list_environment
+  result = {}
+  node[:deploy].each do |_, deploy|
+    deploy[:environment_variables].each do |key, value|
+      result[key] = value
+    end
+  end
+  result
+end
+
 Chef::Log.info("Start sidekiq")
 execute "start sidequk" do
   cwd release_path
   command "bundle exec sidekiq -d -L log/sidekiq.log -C config/sidekiq.yml -e production"
-  environment "RAILS_ENV" => rails_env
+  environment(list_environment)
 end
